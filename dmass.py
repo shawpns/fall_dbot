0e34d2aacffc99a0574e60290e2b6b1a78777733
abhijit shaw <36149425+shaw2018@users.noreply.github.com>
2020-08-19 21:54:55 +0530

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -23,6 +23,54 @@ async def on_command_error(ctx, error):
     if isinstance(error, ignored):
         return
 
+@bot.event
+async def on_member_update(before, after):
+    n = after.nick
+    if n:
+        if n.lower().count("faltu") > 0:
+            last = before.nick
+            if last:
+                await after.edit(nick=last)
+            else:
+                await after.edit(nick="sala kutta")
+                
+                
+@bot.event
+async def on_member_join(member):
+    global joined
+    joined += 1
+    for channel in member.server.channels:
+        if str(channel) == "✺┊ᴡᴇʟᴄᴏᴍᴇ":
+            await channel.send(f"""Welcome to the server {member.mention}""")
+            
+@client.event
+async def on_message(message):
+    global messages
+    messages += 1
+
+    id = client.get_guild(725931924027080725)
+    channels = ["setup"]
+    valid_users = ["ShaW#0768"]
+    bad_words = ["bad", "stop", "45", "kutta", "sala"]
+
+    for word in bad_words:
+        if message.content.count(word) > 0:
+            print("A bad word was said")
+            await message.channel.purge(limit=1)
+
+    if message.content == "+hey":
+        embed = discord.Embed(title="ShaW Ka Area", description="Abe Sale, Chal be")
+        embed.add_field(name="!hello", value="Greets the user")
+        embed.add_field(name="!users", value="Prints number of users")
+        await message.channel.send(content=None, embed=embed)
+
+    if str(message.channel) in channels and str(message.author) in valid_users:
+        if message.content.find("!hello") != -1:
+            await message.channel.send("Hi") 
+        elif message.content == "!users":
+            await message.channel.send(f"""# of Members: {id.member_count}""")
+    
+    
 
 @bot.command(pass_context=True)
 @commands.has_permissions(kick_members=True)
